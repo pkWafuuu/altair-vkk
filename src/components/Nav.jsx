ae6e41ca7bf614ec0bdb5d705aec4b5be4bbfb82
Phil <pk.oaferina@gmail.com>
2023-06-03 16:57:07 +0400

    dark-theme added (unfinished)

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,24 @@
 // eslint-disable-next-line
 
+import { useState } from "react"
+
 function Nav() {
+	const [theme, setTheme] = useState(false)
+
+	function changeTheme() {
+		if(theme === false) {
+			document.body.classList += "dark-theme";
+			setTheme((prev) => !prev);
+		} else {
+			document.body.classList.remove("dark-theme");
+			setTheme((prev) => !prev);
+		}
+	}
+
 	return (
 		<nav>
 			<div className="nav__row">
-				<div className="nav__logo">vkk</div>
+				<button className="nav__logo" onClick={changeTheme}>vkk</button>
 				<ul className="nav__links">
 					<li>
 						<a href="/#" rel="noopener noreferrer" className="nav__link">Home</a>
